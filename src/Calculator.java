792490db203b0d408a94a7f5f9dcd00e74a0f5dd
andreavillagomez <avillagomez@unsa.edu.pe>
2022-05-11 23:50:06 -0500

    Se modifico metodo div

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,9 +8,14 @@
 			  return a * b ;
 		}
 		  int div(int a, int b){
+			  if (b == 0) {
+				  
+				  return 0;
+			  }
 			  return a / b ;
 			  }
 		  int mod(int a, int b){ 
 			  return a % b;
 		}
+		  system out println
 	}
